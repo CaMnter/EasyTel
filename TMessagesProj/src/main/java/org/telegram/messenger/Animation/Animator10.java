c83577cdd59ab8782fa81f14a5b98dfb2341df0f
CaMnter <yuanyu.camnter@gmail.com>
2016-02-08 01:35:27 +0800

    add Animator10 comments

--- TMessagesProj/src/main/java/org/telegram/messenger/Animation/Animator10.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/Animation/Animator10.java
@@ -20,36 +20,86 @@ import android.view.animation.Interpolator;
 
 import java.util.ArrayList;
 
+/**
+ * 修改了 Android3.0的 Animator
+ */
 public abstract class Animator10 implements Cloneable {
 
+    // 缓存 AnimatorListener 集合
     ArrayList<AnimatorListener> mListeners = null;
+    // 缓存 AnimatorPauseListener 集合
     ArrayList<AnimatorPauseListener> mPauseListeners = null;
+
+    // 标记是否可以暂停
     boolean mPaused = false;
 
+    /**
+     * 获得延迟时间
+     *
+     * @return long
+     */
     public abstract long getStartDelay();
 
+    /**
+     * 设置延迟时间
+     *
+     * @param startDelay startDelay
+     */
     public abstract void setStartDelay(long startDelay);
 
+    /**
+     * 设置持续时间
+     *
+     * @param duration duration
+     * @return Animator10
+     */
     public abstract Animator10 setDuration(long duration);
 
+    /**
+     * 设置持续时间
+     *
+     * @return long
+     */
     public abstract long getDuration();
 
+    /**
+     * 设置加速器
+     *
+     * @param value value
+     */
     public abstract void setInterpolator(Interpolator value);
 
+    /**
+     * 判断动画是否正在运行
+     *
+     * @return boolean
+     */
     public abstract boolean isRunning();
 
+    /**
+     * 开始动画
+     */
     public void start() {
 
     }
 
+    /**
+     * 取消动画
+     */
     public void cancel() {
 
     }
 
+    /**
+     * 结束动画
+     */
     public void end() {
 
     }
 
+    /**
+     * 暂停动画
+     */
     @SuppressWarnings("unchecked")
     public void pause() {
         if (isStarted() && !mPaused) {
@@ -64,6 +114,9 @@ public abstract class Animator10 implements Cloneable {
         }
     }
 
+    /**
+     * 恢复动画
+     */
     @SuppressWarnings("unchecked")
     public void resume() {
         if (mPaused) {
@@ -78,18 +131,39 @@ public abstract class Animator10 implements Cloneable {
         }
     }
 
+    /**
+     * 是否暂停了
+     *
+     * @return boolean
+     */
     public boolean isPaused() {
         return mPaused;
     }
 
+    /**
+     * 是否开始了
+     *
+     * @return boolean
+     */
     public boolean isStarted() {
         return isRunning();
     }
 
+    /**
+     * 获得加速器
+     * 默认为null
+     *
+     * @return Interpolator
+     */
     public Interpolator getInterpolator() {
         return null;
     }
 
+    /**
+     * 添加 AnimatorListener
+     *
+     * @param listener listener
+     */
     public void addListener(AnimatorListener listener) {
         if (mListeners == null) {
             mListeners = new ArrayList<AnimatorListener>();
@@ -97,6 +171,11 @@ public abstract class Animator10 implements Cloneable {
         mListeners.add(listener);
     }
 
+    /**
+     * 移除 AnimatorListener
+     *
+     * @param listener listener
+     */
     public void removeListener(AnimatorListener listener) {
         if (mListeners == null) {
             return;
@@ -107,10 +186,20 @@ public abstract class Animator10 implements Cloneable {
         }
     }
 
+    /**
+     * 获取全部 AnimatorListener
+     *
+     * @return ArrayList<AnimatorListener>
+     */
     public ArrayList<AnimatorListener> getListeners() {
         return mListeners;
     }
 
+    /**
+     * 添加 AnimatorPauseListener
+     *
+     * @param listener listener
+     */
     public void addPauseListener(AnimatorPauseListener listener) {
         if (mPauseListeners == null) {
             mPauseListeners = new ArrayList<AnimatorPauseListener>();
@@ -118,6 +207,11 @@ public abstract class Animator10 implements Cloneable {
         mPauseListeners.add(listener);
     }
 
+    /**
+     * 移除 AnimatorPauseListener
+     *
+     * @param listener listener
+     */
     public void removePauseListener(AnimatorPauseListener listener) {
         if (mPauseListeners == null) {
             return;
@@ -128,6 +222,9 @@ public abstract class Animator10 implements Cloneable {
         }
     }
 
+    /**
+     * 清空所有 Listener 缓存
+     */
     public void removeAllListeners() {
         if (mListeners != null) {
             mListeners.clear();
@@ -139,22 +236,40 @@ public abstract class Animator10 implements Cloneable {
         }
     }
 
+    /**
+     * Animator10 的克隆方法
+     *
+     * @return Animator10
+     */
     @Override
     public Animator10 clone() {
         try {
+            // 克隆一份 Animator10 作为副本Animator10
             final Animator10 anim = (Animator10) super.clone();
             if (mListeners != null) {
+                // 保存一份 旧的 ArrayList<AnimatorListener>
                 ArrayList<AnimatorListener> oldListeners = mListeners;
+                // 副本Animator10 的 ArrayList<AnimatorListener>重新初始化
                 anim.mListeners = new ArrayList<AnimatorListener>();
                 int numListeners = oldListeners.size();
+                /*
+                 * 旧的 ArrayList<AnimatorListener> 给
+                 * 副本Animator10 的 ArrayList<AnimatorListener>进行赋值
+                 */
                 for (AnimatorListener oldListener : oldListeners) {
                     anim.mListeners.add(oldListener);
                 }
             }
             if (mPauseListeners != null) {
+                // 保存一份 旧的 ArrayList<AnimatorPauseListener>
                 ArrayList<AnimatorPauseListener> oldListeners = mPauseListeners;
+                // 副本Animator10 的 ArrayList<AnimatorPauseListener>重新初始化
                 anim.mPauseListeners = new ArrayList<AnimatorPauseListener>();
                 int numListeners = oldListeners.size();
+                /*
+                 * 旧的 ArrayList<AnimatorPauseListener> 给
+                 * 副本Animator10 的 ArrayList<AnimatorPauseListener>进行赋值
+                 */
                 for (AnimatorPauseListener oldListener : oldListeners) {
                     anim.mPauseListeners.add(oldListener);
                 }
@@ -165,27 +280,51 @@ public abstract class Animator10 implements Cloneable {
         }
     }
 
+    /**
+     * 设置开始值
+     * 默认不实现
+     */
     public void setupStartValues() {
 
     }
 
+    /**
+     * 设置结束值
+     * 默认不实现
+     */
     public void setupEndValues() {
 
     }
 
+    /**
+     * 设置 target
+     * 默认不实现
+     *
+     * @param target target
+     */
     public void setTarget(Object target) {
 
     }
 
+    /**
+     * 控制 动画的开始、结束、取消和重复
+     */
     public interface AnimatorListener {
         void onAnimationStart(Animator10 animation);
+
         void onAnimationEnd(Animator10 animation);
+
         void onAnimationCancel(Animator10 animation);
+
         void onAnimationRepeat(Animator10 animation);
     }
 
+    /**
+     * 控制 动画的暂停和恢复
+     */
     public interface AnimatorPauseListener {
         void onAnimationPause(Animator10 animation);
+
         void onAnimationResume(Animator10 animation);
     }
 }
